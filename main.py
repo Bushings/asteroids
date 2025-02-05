b1ca94e62698e884a32ed66108e694b6474882fe
Bushings <skventura39@gmail.com>
2025-02-04 22:40:20 -0600

    Finished game

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import pygame
+import sys
 from constants import *
+from player import *
+from shot import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    bullets = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (bullets, updatable, drawable)
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,9 +29,21 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+        #ship.update(dt)
         screen.fill("black")
+        for entity in drawable:
+            entity.draw(screen)
+        #ship.draw(screen)
+        for asteroid in asteroids:
+            for bullet in bullets:
+                if(asteroid.collision(bullet)):
+                    asteroid.split()
+                    bullet.kill()
+            if asteroid.collision(ship):
+                print("Game Over!")
+                sys.exit()
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        print(dt)
 if __name__ == "__main__":
     main()
